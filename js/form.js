08d0531d3b75fcd3831331583f38f1e0efa4469c
xikobe <xikobe@outlook.pt>
2016-02-08 17:36:20 +0000

    submitting

--- js/form.js
+++ js/form.js
@@ -1,10 +1,17 @@
 var app = angular.module('minmax', []);
 
-app.controller('minMaxCtrl', function($scope){
+app.controller('minMaxCtrl', function($scope, $http){
   $scope.formData = {};
 
   $scope.onSubmit = function(){
     console.log("Gonna submit");
     console.log($scope.formData);
+
+    $http.post('https://minmax-server.herokuapp.com/register/', $scope.formData).
+      success(function(data){
+        console.log(":)")
+      }).error(function(data){
+        console.log("error");
+      })
   }
 })
