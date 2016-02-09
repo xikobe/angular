86fdfe57dc51e085e74edf6add0a24a0acd788b6
xikobe <xikobe@outlook.pt>
2016-02-09 14:11:50 +0000

    form validation (core angular)

--- js/form.js
+++ js/form.js
@@ -3,15 +3,23 @@ var app = angular.module('minmax', []);
 app.controller('minMaxCtrl', function($scope, $http){
   $scope.formData = {};
 
-  $scope.onSubmit = function(){
-    console.log("Gonna submit");
-    console.log($scope.formData);
+  $scope.onSubmit = function(valid){
 
-    $http.post('https://minmax-server.herokuapp.com/register/', $scope.formData).
-      success(function(data){
-        console.log(":)")
-      }).error(function(data){
-        console.log("error");
-      })
+    if(valid){
+      console.log("Gonna submit");
+      console.log($scope.formData);
+
+      /*$http.post('https://minmax-server.herokuapp.com/register/', $scope.formData).
+        success(function(data){
+          console.log(":)")
+        }).error(function(data){
+          console.log("error");
+        })*/
+    }else{
+      console.log("invalid");
+    }
+
+
+
   }
 })
