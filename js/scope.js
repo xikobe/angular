f017d170e19d98a07a423accf27230a81d5dc4f0
xikobe <xikobe@outlook.pt>
2016-03-30 20:19:20 +0100

    started scope

---
+++ js/scope.js
@@ -0,0 +1,12 @@
+var app = angular.module('codecraft', []);
+
+app.controller('ParentController', function($scope){
+  $scope.name = "Parent";
+  $scope.reset = function(){
+    $scope.name = "";
+  }
+})
+
+app.controller('ChildController', function($scope){
+  //$scope.name = "Child";
+})
